0848c02ef1956a45452349a583137661955cde13
Nishant1195 <nishant8830875196@gmail.com>
2025-07-24 15:58:21 +0530

    Tailwind and Partial Templates

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // adjust this based on your folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
